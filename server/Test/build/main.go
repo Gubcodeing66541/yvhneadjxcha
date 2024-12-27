2b0e1ec2f9cd4568d413fd8cbc6c926b6bcbd755
Your Name <you@example.com>
2024-12-27 17:45:43 +0800

    u

--- server/Test/build/main.go
+++ server/Test/build/main.go
@@ -4,28 +4,19 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"io"
-	"math/rand"
 	"mime/multipart"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 type responseData struct {
-	Files []struct {
-		Name         string `json:"name"`
-		Size         int    `json:"size"`
-		Type         string `json:"type"`
-		Url          string `json:"url"`
-		DeleteUrl    string `json:"deleteUrl"`
-		ThumbnailUrl string `json:"thumbnailUrl"`
-		DeleteType   string `json:"deleteType"`
-		Fileid       string `json:"fileid"`
-	} `json:"files"`
-	TempFolder interface{} `json:"tempFolder"`
+	Path string `json:"path"`
 }
 
 func main() {
@@ -84,181 +75,95 @@ func main() {
 
 	})
 	s.Run(":80")
-
 }
 
 func GetDomain(i int, filePath string) string {
-	// 要上传的文件路径
-	// 上传接口URL
-	url := "https://stczw.eco-city.gov.cn:10225/fileserver/FileUpload/Upload"
-
-	// 创建一个新的表单文件请求
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-
-	// 添加session_token字段 (可以替换为空或需要的值)
-	_ = writer.WriteField("session_token", "")
-
-	// 打开文件
+	// 打开要上传的文件
 	file, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
+		fmt.Println(fmt.Errorf("failed to open file: %v", err))
 		return ""
 	}
 	defer file.Close()
 
-	// 添加文件字段，文件的字段名为file，文件内容来自file变量，文件名为指定的filename
-	fileName := fmt.Sprintf("%d%d%d.html", time.Now().Unix(), rand.Intn(999), i)
+	// 创建一个缓冲区
+	var requestBody bytes.Buffer
+	writer := multipart.NewWriter(&requestBody)
 
-	part, err := writer.CreateFormFile("file", fileName)
+	// 处理文件字段
+	part, err := writer.CreateFormFile("file", filepath.Base(filePath))
 	if err != nil {
-		fmt.Println("Error creating form file:", err)
+		fmt.Println("failed to create form file: %v", err)
 		return ""
 	}
 
-	// 将文件内容复制到表单中的part
+	// 将文件内容拷贝到 part 中
 	_, err = io.Copy(part, file)
 	if err != nil {
-		fmt.Println("Error copying file:", err)
+		fmt.Println("failed to copy file content: %v", err)
 		return ""
 	}
 
-	// 添加自定义请求头headers，Content-Type: image/jpeg
-	err = writer.WriteField("headers", "Content-Type: image/jpeg")
+	// 添加其他字段
+	err = writer.WriteField("imgName", "wxfile://tmp_17187c3d0c60b0e608753587765ccd4f.jpg")
 	if err != nil {
-		fmt.Println("Error writing field:", err)
+		fmt.Println("failed to write imgName: %v", err)
+		return ""
+	}
+	err = writer.WriteField("userId", "45842")
+	if err != nil {
+		fmt.Println("failed to write userId: %v", err)
 		return ""
 	}
 
-	// 结束并关闭writer，完成multipart/form-data的构建
+	// 关闭写入器
 	err = writer.Close()
 	if err != nil {
-		fmt.Println("Error closing writer:", err)
+		fmt.Println("failed to close writer: %v", err)
 		return ""
 	}
 
-	// 创建请求
-	req, err := http.NewRequest("POST", url, body)
+	// 创建 HTTP 请求
+	req, err := http.NewRequest("POST", "https://www.ldt.gov.cn/appletController/uploadWXPic", &requestBody)
 	if err != nil {
-		fmt.Println("Error creating request:", err)
+		fmt.Println("failed to create request: %v", err)
 		return ""
 	}
 
 	// 设置请求头
-	req.Header.Set("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 18_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.54(0x18003635) NetType/WIFI Language/zh_CN")
+	req.Header.Set("Reqable-Id", "reqable-id-aad5858c-15eb-4f8b-bf02-4eab027285d0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 18_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.54(0x18003637) NetType/WIFI Language/zh_CN")
 	req.Header.Set("Accept-Encoding", "gzip,compress,br,deflate")
-	req.Header.Set("Content-Type", writer.FormDataContentType()) // multipart/form-data
-	req.Header.Set("Referer", "https://servicewechat.com/wx98f0c636dce332fe/95/page-frame.html")
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.Header.Set("TOKEN", "61BB8C4C11D622EB0C2426FD178A9F85")
+	req.Header.Set("Referer", "https://servicewechat.com/wx28c943968e60a1fc/11/page-frame.html")
 
-	// 使用http客户端发送请求
+	// 执行请求
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error sending request:", err)
+		fmt.Println("failed to execute request: %v", err)
 		return ""
 	}
 	defer resp.Body.Close()
 
-	// 打印响应内容
-	fmt.Println("Response Status:", resp.Status)
-	respBody, err := io.ReadAll(resp.Body)
+	// 读取响应
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Error reading response body:", err)
+		fmt.Println("failed to read response body: %v", err)
 		return ""
 	}
-	fmt.Println("Response Body:", string(respBody))
 
-	var respd responseData
-	err = json.Unmarshal(respBody, &respd)
-	if err != nil {
-		fmt.Println("Error unmarshalling response body:", err)
-	}
-	return "https://stczw.eco-city.gov.cn:10225/fileserver" + respd.Files[0].Url
+	// 输出响应内容
+	fmt.Println("Response Status:", resp.Status)
+	fmt.Println("Response Body:", string(body))
+	var rp responseData
+	json.Unmarshal(body, &rp)
+
+	return rp.Path
+
 }
 
 func ResetDomain(fileName string) string {
-	// 要上传的文件路径
-	filePath := "tezt.html"
-	// 上传接口URL
-	url := "https://stczw.eco-city.gov.cn:10225/fileserver/FileUpload/Upload"
-
-	// 创建一个新的表单文件请求
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-
-	// 添加session_token字段 (可以替换为空或需要的值)
-	_ = writer.WriteField("session_token", "")
-
-	// 打开文件
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return ""
-	}
-	defer file.Close()
-
-	part, err := writer.CreateFormFile("file", fileName)
-	if err != nil {
-		fmt.Println("Error creating form file:", err)
-		return ""
-	}
-
-	// 将文件内容复制到表单中的part
-	_, err = io.Copy(part, file)
-	if err != nil {
-		fmt.Println("Error copying file:", err)
-		return ""
-	}
-
-	// 添加自定义请求头headers，Content-Type: image/jpeg
-	err = writer.WriteField("headers", "Content-Type: image/jpeg")
-	if err != nil {
-		fmt.Println("Error writing field:", err)
-		return ""
-	}
-
-	// 结束并关闭writer，完成multipart/form-data的构建
-	err = writer.Close()
-	if err != nil {
-		fmt.Println("Error closing writer:", err)
-		return ""
-	}
-
-	// 创建请求
-	req, err := http.NewRequest("POST", url, body)
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return ""
-	}
-
-	// 设置请求头
-	req.Header.Set("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 18_1_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.54(0x18003635) NetType/WIFI Language/zh_CN")
-	req.Header.Set("Accept-Encoding", "gzip,compress,br,deflate")
-	req.Header.Set("Content-Type", writer.FormDataContentType()) // multipart/form-data
-	req.Header.Set("Referer", "https://servicewechat.com/wx98f0c636dce332fe/95/page-frame.html")
-
-	// 使用http客户端发送请求
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return ""
-	}
-	defer resp.Body.Close()
-
-	// 打印响应内容
-	fmt.Println("Response Status:", resp.Status)
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return ""
-	}
-	fmt.Println("Response Body:", string(respBody))
-
-	var respd responseData
-	err = json.Unmarshal(respBody, &respd)
-	if err != nil {
-		fmt.Println("Error unmarshalling response body:", err)
-	}
-	return "https://stczw.eco-city.gov.cn:10225/fileserver" + respd.Files[0].Url
+	return ""
 }
